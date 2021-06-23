131a2bb4ce73af3262187994870373148561bbba
Quickeman <finnquicke@hotmail.com>
2021-06-23 17:22:12 +0100

    Added CustomGenerator class for generating waveforms from a user-defined generator expression

---
+++ src/custom.cpp
@@ -0,0 +1,13 @@
+#include "custom.h"
+
+using namespace std;
+
+CustomGenerator::CustomGenerator(
+    const float_t sample_rate,
+    function<float_t(float_t)> expr,
+    float_t phaseMin,
+    float_t phaseMax):
+    PitchedGenerator(sample_rate) {
+    generator = expr;
+    phase.setRange(phaseMin, phaseMax);
+}
---
+++ src/custom.h
@@ -0,0 +1,26 @@
+#ifndef _SIGGEN_CUSTOM_H
+#define _SIGGEN_CUSTOM_H
+
+#include "base_classes.h"
+
+/** Class for making custom signal generator. */
+class CustomGenerator : public PitchedGenerator {
+public:
+    /** Constructor.
+     * @param sample_rate sample rate of the generator.
+     * @param expr generator expression (e.g. a lambda) called to make samples.
+     * @param phaseMin minimum phase to pass as input to `expr`. Default 0.
+     * @param phaseMax maximum phase to pass as input to `expr`. Default 1.
+     */
+    CustomGenerator(
+        const float_t sample_rate,
+        std::function<float_t(float_t)> expr,
+        float_t phaseMin = 0.0,
+        float_t phaseMax = 1.0
+    );
+
+    /** Default destructor. */
+    ~CustomGenerator() = default;
+};
+
+#endif
