c534dece8e3da1e372b87d5586a9716628bc0c1c
Quickeman <finnquicke@hotmail.com>
2021-06-24 17:06:19 +0100

    Added custom signal tests

---
+++ test/custom_signal.cpp
@@ -0,0 +1,36 @@
+#include "custom_signal.h"
+
+#include "unit_test_framework.h"
+
+#include "functions.h"
+
+using namespace test;
+
+int main() {
+    float_t sr = 10.0;
+    size_t bl = sr + 1e-6;
+
+    auto lambda = [](float_t x) {
+        return x * x;
+    };
+
+    float_t pMin = -1.0;
+    float_t pMax = 1.0;
+
+    CustomGenerator gen(sr, lambda, pMin, pMax);
+
+    std::vector<float_t> r = gen.generate(bl, 1.0);
+
+    float_t ph = pMin;
+    float_t expt = lambda(ph);
+
+    for (int i = 0; i < bl; i++) {
+        check(
+            maths::isNear(r[i], expt),
+            "Unexpected value " + std::to_string(r[i]) + " at index "
+            + std::to_string(i) + ", expected " + std::to_string(expt)
+        );
+        ph += 0.2;
+        expt = lambda(ph);
+    }
+}
